4ea582d9aa2241487162e24ba3240462f468aade
Koray Aydemir <korayaydemir276@gmail.com>
2023-04-21 22:54:59 +0300

    sane config for jest

--- find-a-peak/src/app.test.ts
+++ find-a-peak/src/app.test.ts
@@ -11,3 +11,26 @@ test("Should return a peak when peak is in the middle in even length array. And 
 })
 
 
+test("Should return last index in an ascending order array.", () => {
+  const result = findPeak([1, 2, 3, 4, 5]);
+  expect(result).toEqual({ peak: 5, index: 4 })
+})
+
+test("Should return first index in a descending order array.", () => {
+  const result = findPeak([5, 4, 3, 2, 1]);
+  expect(result).toEqual({ peak: 5, index: 0 })
+})
+
+test("Should return a peak from right when only right side is bigger", () => {
+  const result = findPeak([1, 2, 4, 7, 3]);
+  expect(result).toEqual({ peak: 7, index: 3 })
+})
+
+
+test("Should return a peak from left when only left side is bigger", () => {
+  const result = findPeak([1, 6, 2, 2, 4]);
+  expect(result).toEqual({ peak: 6, index: 1 })
+})
+
+
+
