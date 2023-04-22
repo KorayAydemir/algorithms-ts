65bd18ab2a23f0b5aac5cf16ba90dcffea3978ea
Koray Aydemir <korayaydemir276@gmail.com>
2023-04-22 18:22:54 +0300

    add tail call recursive fib

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  verbose: true,
+  silent: true, // disable all console output
+  transform: {
+    '^.+\\.(ts|tsx)?$': ['ts-jest', { diagnostics: { ignoreCodes: ['TS151001'] } }],
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  testPathIgnorePatterns: [".d.ts", ".js"]
 };
